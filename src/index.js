967ccc3eae1e057fbaf8fbc1d811f4b746834663
Ann-DevCommit <var.ann.box@gmail.com>
2021-11-14 12:32:20 +0200

    hw with logs

--- src/index.js
+++ src/index.js
@@ -8,24 +8,7 @@ import countryCardTemplate from './templates/country-card.hbs'
 import './partials/country-search.html'
 
 
-// console.log(alert)
-// console.log(error)
 
-
-
-  
-// const country = 'sp'
-
-// fetch(`https://restcountries.com/v2/name/${country}`)
-//     .then(response => response.json())
-//     .then(r => console.log(r))
-//     .catch(error => console.log("error", error));
-    
-// fetch(`https://restcountries.com/v2/name/Macau`)
-//     .then(response => response.json())
-//     .then(r => console.log(r))
-//     .catch(error => console.log("error", error));
-    
 const inputEl = document.querySelector('.country-input')
 const resultContainerEl = document.querySelector('.js-result-container')
 
@@ -44,63 +27,54 @@ function onCountrySearch(event) {
     if (!inputContryName)
         return;
 
-    fetchCountries(inputContryName).then(
-        function responseError(countries) {
-            if (countries.status === 404) {
+    fetchCountries(inputContryName)
+        .then(
+            function responseError(countries) {
+                    if (countries.status === 404) {
                         errorMessage('404, Таких стран не бывает :)')
                         console.log('404, Таких стран не бывает :)')
                         return
-                        
-                    }
+                   }
                     
-                    // countries => {
-            const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(inputContryName))
+                    
+                const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(inputContryName))
                 console.log(countries)
                 console.log(filteredCountries)
                 return filteredCountries
             }
-                // }
-        
-            
-     )
-         .then(renderCountries)
-         .catch(error => console.log('catch error. Страна не найдена', error));
-    
-    
-    
-    
-    
+        )
+        .then(renderCountries)
+        .catch(error => console.log('catch error. Страна не найдена', error));
 }
     
 
-// function getApi (inputContryName){
-//      fetch(`https://restcountries.com/v2/name/${inputContryName}`)
-//          .then(response => response.json())
-//          .then(countries => {
-//                          if (countries.status === 404){
-//                          console.log('404, Таких стран не бывает :)')
-//                          return
-//                         }
-//                     }
-             
-//                 )
-//          .catch(error => console.log('Страна не найдена', error));
-// }
-      
 
 
 function renderCountries(filteredCountries) {
     if (filteredCountries.length === 1) {
 
-        // { name } = filteredCountries;
-        // resultContainerEl.insertAdjacentElement('beforeend', countryCardTemplate(...filteredCountries))
-        // countryCardTemplate(...filteredCountries)
-        console.log(filteredCountries)
+        const countryCardToRender = countryCardTemplate(...filteredCountries)
+        console.log(countryCardTemplate(...filteredCountries))
+        console.log(countryCardTemplate(filteredCountries))
+
+        console.log(countryCardToRender)
+        console.log(typeof countryCardToRender)
+        // resultContainerEl.insertAdjacentHTML('beforeend', countryCardToRender)
+        resultContainerEl.innerHTML = countryCardToRender
+        
         return
     }
     if (((filteredCountries.length >= 2) && (filteredCountries.length <= 10))) {
-        resultContainerEl.insertAdjacentElement('beforeend', countryCardTemplate(...filteredCountries))
+
         console.log(filteredCountries)
+        console.log(typeof filteredCountries)
+
+        const countryListToRender = countryListTemplate(filteredCountries)
+        console.log(countryListToRender)
+        console.log(typeof countryListToRender)
+                // resultContainerEl.insertAdjacentHTML('beforeend', countryListToRender)
+        resultContainerEl.innerHTML = countryListToRender
+        
         return
     }
     if ((filteredCountries.length < 1)) {
@@ -114,41 +88,18 @@ function renderCountries(filteredCountries) {
     }
 }
 
+
+
 function errorMessage(message) {
     error({
+            addClass: 'modal',
             text: `${message}`,
             title: false,
             icon: false,
             closer: false,
             sticker: false,
-            delay: 1000,
+            delay: 0,
             });
 }
 
 
-// function chackCommonName(countries, inputContryName) {
-//     const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(`${inputContryName}`))
-//     if (!filteredCountries) {
-//         alert('Страна не найдена')
-//         return
-//     }
-//     return console.log(inputContryName)
-// }
-
-// console.log(2 <= 1 )
-
-// function getApi() {
-//     fetch(`https://restcountries.com/v2/name/${country}`)
-//     .then(response => response.json())
-//     .then(r => console.log(r))
-//     .catch(error => console.log("error", error));
-// }
-const users = [
-  { id: '000', name: 'Mango', isActive: true },
-  { id: '001', name: 'Poly', isActive: false },
-  { id: '002', name: 'Ajax', isActive: true },
-  { id: '003', name: 'Chelsey', isActive: false },
-];
-
-const any = users.filter(user => user.name.toLowerCase().includes('hhhhhhan'))
-console.log(any)
