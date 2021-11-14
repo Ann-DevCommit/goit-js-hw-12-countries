73dab0fa8999058de49f1c1a511d8f538f819b95
Ann-DevCommit <var.ann.box@gmail.com>
2021-11-14 12:45:18 +0200

    hw deleted logs

--- src/index.js
+++ src/index.js
@@ -12,17 +12,11 @@ import './partials/country-search.html'
 const inputEl = document.querySelector('.country-input')
 const resultContainerEl = document.querySelector('.js-result-container')
 
-console.log(inputEl)
-console.log(resultContainerEl)
-
 inputEl.addEventListener('input', debounce(onCountrySearch, 500))
-
-
 
 function onCountrySearch(event) {
 
     const inputContryName = event.target.value
-    console.log(event.target.value)
 
     if (!inputContryName)
         return;
@@ -32,14 +26,11 @@ function onCountrySearch(event) {
             function responseError(countries) {
                     if (countries.status === 404) {
                         errorMessage('404, Таких стран не бывает :)')
-                        console.log('404, Таких стран не бывает :)')
                         return
                    }
                     
                     
                 const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(inputContryName))
-                console.log(countries)
-                console.log(filteredCountries)
                 return filteredCountries
             }
         )
@@ -54,37 +45,20 @@ function renderCountries(filteredCountries) {
     if (filteredCountries.length === 1) {
 
         const countryCardToRender = countryCardTemplate(...filteredCountries)
-        console.log(countryCardTemplate(...filteredCountries))
-        console.log(countryCardTemplate(filteredCountries))
-
-        console.log(countryCardToRender)
-        console.log(typeof countryCardToRender)
-        // resultContainerEl.insertAdjacentHTML('beforeend', countryCardToRender)
         resultContainerEl.innerHTML = countryCardToRender
-        
         return
     }
     if (((filteredCountries.length >= 2) && (filteredCountries.length <= 10))) {
 
-        console.log(filteredCountries)
-        console.log(typeof filteredCountries)
-
         const countryListToRender = countryListTemplate(filteredCountries)
-        console.log(countryListToRender)
-        console.log(typeof countryListToRender)
-                // resultContainerEl.insertAdjacentHTML('beforeend', countryListToRender)
         resultContainerEl.innerHTML = countryListToRender
-        
         return
     }
     if ((filteredCountries.length < 1)) {
-        errorMessage('Таких стран не бывает :)')
-        console.log('Таких стран не бывает :)')
-        
+        errorMessage('Таких стран не бывает :)')        
     }
     else {
         errorMessage('Введите более специфичное значение')
-        console.log('Введите более специфичное значение')
     }
 }
 
